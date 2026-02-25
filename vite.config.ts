f9ea14cbb28945ba4f62ae8fe4cba1b68ea7f0ee
RassoulEbrahimi <rasoul.e2020@gmail.com>
2026-02-25 15:02:37 +0100

    chore: update Vite build configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,14 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    base: '/mydailyflow/',   // ← این خط خیلی مهمه!
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+export default defineConfig({
+  base: '/mydailyflow/',   // ← دقیقاً همین باید باشه (با slash آخر)
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-    },
-  };
+  },
 });
